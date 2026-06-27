641f31412d217731713d0f6fbde6c09b781c3fd9
Israfil Hossen <sisrafilss@gmail.com>
2026-06-27 21:46:52 +0600

    66-1 (partial)

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { Hero } from "@/components/modules/Hero";
+import Head from "next/head";
+
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+       <Head>
+        <title>AI-Powered Healthcare - Find Your Perfect Doctor</title>
+        <meta
+          name="description"
+          content="Discover top-rated doctors tailored to your needs with our AI-powered healthcare platform. Get personalized recommendations and book appointments effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Hero />
+      </main>
+    </>
   );
 }
