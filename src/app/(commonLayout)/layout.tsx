9df42c58adf04b41629f650722257030f1bb8fb2
Israfil Hossen <sisrafilss@gmail.com>
2026-06-27 16:46:07 +0600

    project initialize

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,13 @@
+import PublicNavbar from '@/components/shared/PublicNavbar'
+import React from 'react'
+
+const CommonLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <>
+    <PublicNavbar />
+        {children}
+    </>
+  )
+}
+
+export default CommonLayout
---
+++ src/components/shared/PublicNavbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const PublicNavbar = () => {
+  return (
+    <div>
+    <div>PublicNavbar</div>
+    <Button>Test Button</Button>
+    </div>
+  )
+}
+
+export default PublicNavbar
